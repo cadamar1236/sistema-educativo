57153bf20121ef7a34aa5090a5afd4f08309290d
cadamar1236 <73383629+cadamar1236@users.noreply.github.com>
2025-09-15 17:50:20 +0200

    fix: Eliminar componentes MathDemo de producción - eran solo para testing

--- julia-frontend/pages/math-demo.tsx
+++
@@ -1,9 +0,0 @@
-import MathDemo from '@/components/MathDemo';
-
-export default function MathDemoPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <MathDemo />
-    </div>
-  );
-}
